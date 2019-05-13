74adc0291c6fad52ec3a71319262fad8aa8ab233
Jhonatan Parra <jhonatanhudson@gmail.com>
2019-05-13 17:38:59 -0400

    changes

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => <div className="footer text-center"><i class="fab fa-react"></i></div>;
+
+export default Footer;
---
+++ src/images.js
@@ -0,0 +1,29 @@
+import homer from './images/homer.jpg';
+import marge from './images/marge2.jpg';
+import milhouse from './images/milhouse-van-houten-profile.png';
+import Skinner from './images/skinner.jpeg';
+import bart from './images/bart-simpson.jpg';
+import fatTony from './images/fat-tony.png';
+import willie from './images/willie.jpg';
+import edna from './images/edna2.jpg';
+import krusty from './images/krusty.png';
+import moe from './images/moe.jpg';
+import lenny from './images/lennyleonard.jpg';
+import ned from './images/ned.jpg';
+
+const images = [
+  homer,
+  marge,
+  milhouse,
+  Skinner,
+  bart,
+  fatTony,
+  willie,
+  edna,
+  krusty,
+  moe,
+  lenny,
+  ned
+];
+
+export default images;
